3b93ed0e8469603cdd5a18074e354e3dd86d6c94
matveikulakovskiii <your-email@example.com>
2024-09-25 15:39:50 +0300

    add some adding games

--- Index.js
+++ Index.js
@@ -1,9 +1,13 @@
-const app = require('express')()
-const port = 8080
+const express = require('express')
+const res = require('express/lib/response')
 const req = require('express/lib/request')
+const app = express()
+const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require("yamljs")
 const swaggerDocument = yamljs.load('./docs/swagger.yaml')
+
+app.use(express.json())
 
 const games = [
     {id: 1, name: "Witcher 3", price: 29.99},
@@ -25,11 +29,20 @@ app.get('/games/:id', (req, res) => {
 })
 
 app.post('/games', (req, res) => {
-    games.push({
-        id: games.length - 1,
+    if (!req.body.name || !req.body.price){
+        return res.status(400).send({error: 'One or all params are missing'})
+    }
+    let game = {
+        id: games.length + 1,
         price: req.body.price,
         name: req.body.name
-    })
+    }
+
+    games.push(game)
+
+    res.status(201)
+        .location('${getBaseUrl(req)}/games${games.lemgth}')
+        .send(game)
 })
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
@@ -37,3 +50,8 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
 })
+
+function getBaseUrl(req){
+    return req.connection && req.connection.encrypted
+        ? 'https' : 'http' + '://${req.headers.host}'
+}
